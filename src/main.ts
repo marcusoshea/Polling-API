ebd6de580a80db151174c7e5730ba9b2775f0d95
Marcus O'Shea <marcusoshea100@gmail.com>
2022-11-10 20:44:04 -0500

    Upgraded to nest 9 and added swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
@@ -11,6 +12,18 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
+
+  const configsw = new DocumentBuilder()
+  .setTitle('PollingAPI')
+  .setDescription('The polling API description')
+  .setVersion('1.0')
+  .addTag('polling')
+  .build();
+const document = SwaggerModule.createDocument(app, configsw);
+SwaggerModule.setup('api', app, document);
+
+
+
   await app.listen(port, () => {
     console.log('[WEB]', config.get<string>('BASE_URL'));
   });
